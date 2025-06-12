7fb90840c1da00537ef33db63ad746f6216db573
Training8767 <training8767@gmail.com>
2025-06-12 13:08:08 +0530

    first commit

--- app.py
+++ app.py
@@ -8,32 +8,25 @@ from datetime import datetime
 import qrcode
 from io import BytesIO
 import base64
-from dotenv import load_dotenv
-import os
+import streamlit as st
 
-# === 🔐 Load environment variables from .env ===
-load_dotenv()
-
-
-# Fix: load private_key separately and convert \n to real newlines
-private_key_raw = os.getenv("PRIVATE_KEY")
+private_key_raw = st.secrets["PRIVATE_KEY"]
 private_key = private_key_raw.replace('\\n', '\n') if private_key_raw else None
 
 
 service_account_info = {
-    "type": os.getenv("TYPE"),
-    "project_id": os.getenv("PROJECT_ID"),
-    "private_key_id": os.getenv("PRIVATE_KEY_ID"),
+    "type": st.secrets["TYPE"],
+    "project_id": st.secrets["PROJECT_ID"],
+    "private_key_id": st.secrets["PRIVATE_KEY_ID"],
     "private_key": private_key,
-    "client_email": os.getenv("CLIENT_EMAIL"),
-    "client_id": os.getenv("CLIENT_ID"),
-    "auth_uri": os.getenv("AUTH_URI"),
-    "token_uri": os.getenv("TOKEN_URI"),
-    "auth_provider_x509_cert_url": os.getenv("AUTH_PROVIDER_CERT_URL"),
-    "client_x509_cert_url": os.getenv("CLIENT_CERT_URL"),
-    "universe_domain": os.getenv("UNIVERSE_DOMAIN")
+    "client_email": st.secrets["CLIENT_EMAIL"],
+    "client_id": st.secrets["CLIENT_ID"],
+    "auth_uri": st.secrets["AUTH_URI"],
+    "token_uri": st.secrets["TOKEN_URI"],
+    "auth_provider_x509_cert_url": st.secrets["AUTH_PROVIDER_CERT_URL"],
+    "client_x509_cert_url": st.secrets["CLIENT_CERT_URL"],
+    "universe_domain": st.secrets.get("UNIVERSE_DOMAIN")  # optional, might be missing
 }
-
 
 credentials = service_account.Credentials.from_service_account_info(
     service_account_info,
